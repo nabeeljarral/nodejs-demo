10761e4045a9d4a3955f28f2b321291d145eb794
nabeeljarral <40709574+nabeeljarral@users.noreply.github.com>
2023-12-13 01:03:21 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 
+'use strict';
+const express = require('express');
+const PORT = 8080;
+const HOST = '0.0.0.0';
+const app= express();
+app.get('/', (reg, res) => {
+  res.send('Hello world');
+}
+);
+
+app.listen(PORT, Host, () => {
+   console.log('Running on http://${HOST}:${PORT}');
+  
+});
